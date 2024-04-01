3ed4f7efd1637eaa6f6948c3c33345d205a91e8c
Gunārs Ābeltiņš <gunars2003@gmail.com>
2024-04-01 13:36:10 +0300

    Add custom hash table

--- 02_Recenzenti/main.cpp
+++ 02_Recenzenti/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-#include <map>
-
 using namespace std;
 
 #define MAX_NAME_LEN 100
 #define MAX_PSEUDONYM_CNT 50
+#define MAX_REVIEWER_CNT 10000
 
 struct String
 {
@@ -116,13 +115,148 @@ struct Reviewer
     }
 };
 
+long long polynomial_hash(const char *str, long long p = 31, long long m = 1e9 + 9)
+{
+    long long hash = 0;
+    long long p_pow = 1;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        hash = (hash + (str[i] - '0' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+
+    return hash;
+}
+
+long long get_hash(const String &str)
+{
+    return polynomial_hash(str.data);
+}
+
+long long get_hash(const int &num)
+{
+    return num;
+}
+
+template <typename Key, typename Value>
+class HashTable
+{
+    struct Node
+    {
+        Key key;
+        Value value;
+        Node *next;
+    };
+
+    Node **table;
+    int size;
+
+    int hash(Key key) const
+    {
+        return get_hash(key) % size;
+    }
+
+public:
+    HashTable(int bucket_count)
+    {
+        this->size = bucket_count;
+        table = new Node *[bucket_count];
+        for (int i = 0; i < bucket_count; ++i)
+        {
+            table[i] = nullptr;
+        }
+    }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            Node *node = table[i];
+            while (node != nullptr)
+            {
+                Node *next = node->next;
+                delete node;
+                node = next;
+            }
+        }
+
+        delete[] table;
+    }
+
+    void insert(Key key, Value value)
+    {
+        int index = hash(key);
+        Node *node = table[index];
+        while (node != nullptr)
+        {
+            if (node->key == key)
+            {
+                node->value = value;
+                return;
+            }
+
+            node = node->next;
+        }
+
+        node = new Node();
+        node->key = key;
+        node->value = value;
+        node->next = table[index];
+        table[index] = node;
+    }
+
+    Value *find(Key key)
+    {
+        int index = hash(key);
+        Node *node = table[index];
+        while (node != nullptr)
+        {
+            if (node->key == key)
+            {
+                return &node->value;
+            }
+
+            node = node->next;
+        }
+
+        return nullptr;
+    }
+
+    void erase(Key key)
+    {
+        int index = hash(key);
+        Node *node = table[index];
+        Node *prev = nullptr;
+        while (node != nullptr)
+        {
+            if (node->key == key)
+            {
+                if (prev == nullptr)
+                {
+                    table[index] = node->next;
+                }
+                else
+                {
+                    prev->next = node->next;
+                }
+
+                delete node;
+                return;
+            }
+
+            prev = node;
+            node = node->next;
+        }
+    }
+};
+
 int main()
 {
     (void)!freopen("reviewers.in", "r", stdin);
     (void)!freopen("reviewers.out", "w", stdout);
 
-    map<int, String> pseudonyms;
-    map<String, Reviewer> reviewers;
+    HashTable<int, String> pseudonyms(MAX_REVIEWER_CNT * MAX_PSEUDONYM_CNT);
+    HashTable<String, Reviewer> reviewers(MAX_REVIEWER_CNT);
 
     char prefix;
     while (cin >> prefix)
@@ -137,20 +271,20 @@ int main()
                 cin >> new_pseudonyms[i];
             }
 
-            if (reviewers.find(name) == reviewers.end())
+            if (reviewers.find(name) == nullptr)
             {
                 Reviewer reviewer(name);
-                reviewers.insert({name, reviewer});
+                reviewers.insert(name, reviewer);
             }
 
-            Reviewer &reviewer = reviewers[name];
+            Reviewer &reviewer = *(reviewers.find(name));
 
             bool can_insert = true;
             for (int i = 0, cnt = 0; i < new_pseudonym_count; ++i)
             {
-                if (pseudonyms.find(new_pseudonyms[i]) != pseudonyms.end())
+                if (pseudonyms.find(new_pseudonyms[i]) != nullptr)
                 {
-                    if (pseudonyms[new_pseudonyms[i]] == name)
+                    if (*(pseudonyms.find(new_pseudonyms[i])) == name)
                     {
                         ++cnt;
                     }
@@ -172,9 +306,9 @@ int main()
             {
                 for (int i = 0; i < new_pseudonym_count; ++i)
                 {
-                    if (pseudonyms.find(new_pseudonyms[i]) == pseudonyms.end())
+                    if (pseudonyms.find(new_pseudonyms[i]) == nullptr)
                     {
-                        pseudonyms.insert({new_pseudonyms[i], name});
+                        pseudonyms.insert(new_pseudonyms[i], name);
                         reviewer.pseudonyms[reviewer.pseudonym_count++] = new_pseudonyms[i];
                     }
                 }
@@ -191,10 +325,10 @@ int main()
             int pseudonym;
             cin >> pseudonym;
 
-            if (pseudonyms.find(pseudonym) != pseudonyms.end())
+            if (pseudonyms.find(pseudonym) != nullptr)
             {
-                String name = pseudonyms[pseudonym];
-                Reviewer &reviewer = reviewers[name];
+                String name = *(pseudonyms.find(pseudonym));
+                Reviewer &reviewer = *(reviewers.find(name));
 
                 for (int i = 0; i < reviewer.pseudonym_count; ++i)
                 {
@@ -215,9 +349,9 @@ int main()
             int pseudonym;
             cin >> pseudonym;
 
-            if (pseudonyms.find(pseudonym) != pseudonyms.end())
+            if (pseudonyms.find(pseudonym) != nullptr)
             {
-                cout << pseudonyms[pseudonym].data << endl;
+                cout << pseudonyms.find(pseudonym)->data << endl;
             }
             else
             {
